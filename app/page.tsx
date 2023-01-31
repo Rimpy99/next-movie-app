93efa67e1c36cfb0607e052a6db53c9e67ab3a5a
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2023-01-31 10:19:06 +0100

    Movies component created

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+import Movies from './Movies';
+
 export default function Home() {
   return (
     <main>
       <h1 className='text-red-500'>main page</h1>
+      <Movies/>
     </main>
   )
 }
